f7d4e7396dee74100013bc7669ce5e134e294572
Mouad Laayouni <108269816+mouadlaayouni-youcan@users.noreply.github.com>
2022-08-25 11:33:00 +0100

    change route names and add get categories list route

--- FlaskAPI/api/controller.py
+++ FlaskAPI/api/controller.py
@@ -7,15 +7,23 @@ import numpy as np
 from tensorflow.python.keras.models import load_model
 
 
+categories = ['Arts Crafts and Sewing',
+              'Beauty',
+              'Clothing Shoes and Jewelry',
+              'Electronics',
+              'Grocery and Gourmet Food',
+              'Others',
+              'Toys and Games']
+
+
 def predict(json_data, predict_proba=False):
     df = pd.DataFrame(json_data, columns=['title', 'description'])
     if df['title'].isnull().sum() > 0:
         return {'message': 'title is obligatory'}, 400
     df_combined = combineColumns(df)
     df_processed = preprocess(df_combined)
-    # df = df.reset_index(drop=True)
-    vectorizer = pickle.load(open('TF-IDF.pkl', 'rb'))
-    data = vectorizer.transform(df_processed["title"]).toarray()
+    vectorize = pickle.load(open('TF-IDF.pkl', 'rb'))
+    data = vectorize.transform(df_processed["title"]).toarray()
     print(data.shape)
     df_processed = pd.DataFrame(data)
     model = load_model('ANN-N.h5')
@@ -24,11 +32,13 @@ def predict(json_data, predict_proba=False):
     for i in range(len(prediction)):
         result.append(categories[np.argmax(prediction[i])])
     if predict_proba:
-        # pred_proba = model.predict_proba(df_processed)
         proba = np.round(prediction[0], 3)
-        return json.dumps(result[0]), proba
+        cat_proba = []
+        for i in range(len(categories)):
+            cat_proba.append({categories[i]: str(proba[i])})
+        print(result[0], cat_proba)
+        return {'prediction': result[0], 'proba': cat_proba}
     else:
-        # return json.dumps(result, cls=NumpyArrayEncoder)
         json_result = []
         for i in range(len(result)):
             json_result.append({'title': df['title'][i], 'category': result[i]})
@@ -36,29 +46,5 @@ def predict(json_data, predict_proba=False):
         return json.dumps(json_result)
 
 
-# categories = ['Automotive',
-#               'Pet Supplies',
-#               'Sports & Outdoors',
-#               'Beauty',
-#               'Health & Personal Care',
-#               'Arts, Crafts & Sewing',
-#               'Cell Phones & Accessories',
-#               'Toys & Games',
-#               'Baby Products',
-#               'Clothing, Shoes & Jewelry',
-#               'Appliances',
-#               'Musical Instruments',
-#               'Electronics',
-#               'Tools & Home Improvement',
-#               'Industrial & Scientific',
-#               'Office Products',
-#               'Grocery & Gourmet Food',
-#               'Patio, Lawn & Garden']
-
-categories = ['Arts Crafts and Sewing',
-              'Beauty',
-              'Clothing Shoes and Jewelry',
-              'Electronics',
-              'Grocery and Gourmet Food',
-              'Others',
-              'Toys and Games']
+def getCategories():
+    return categories
--- FlaskAPI/api/routes.py
+++ FlaskAPI/api/routes.py
@@ -1,24 +1,33 @@
 from flask import request
-from api.controller import predict
+from api.controller import predict, getCategories
 import warnings
 from api import app
 
 warnings.filterwarnings("ignore")
 
 
-@app.route('/categories', methods=['POST'])
-def process_json():
+@app.route('/categorise', methods=['POST'])
+def categoriseMultipleProducts():
+
     content_type = request.headers.get('Content-Type')
+
     if content_type == 'application/json':
         return predict(request.json)
     else:
         return 'Content-Type not supported!', 400
 
 
-@app.route('/prediction', methods=['GET'])
-def proba_pred():
-    if request.args.get('title') is None:
-        return "title is obligatory", 400
-    prediction, proba = predict([{'title': request.args.get('title'), 'description': request.args.get('description')}],
-                                True)
-    return {'prediction': prediction, 'proba': str(proba)}
+@app.route('/categorise/probability', methods=['POST'])
+def categoriseProductWithProbability():
+
+    content_type = request.headers.get('Content-Type')
+
+    if content_type == 'application/json':
+        return predict(request.json, True)
+    else:
+        return 'Content-Type not supported!', 400
+
+
+@app.route('/categories/list', methods=['GET'])
+def getAllCategories():
+    return getCategories()
